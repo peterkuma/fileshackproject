0501b080655c5f135d715a6834c25b4ec56e37bd
Peter Kuma <peterkuma@centrum.sk>
2012-03-22 15:07:46 +0100

    Add settings_local.py to repository.

---
+++ fileshackproject/settings_local.py
@@ -0,0 +1,16 @@
+DEBUG = False
+
+ADMINS = (
+    ('Peter Kuma', 'peto.kuma@gmail.com'),
+)
+
+#TIME_ZONE = 'UTC'
+
+#MEDIA_ROOT = '/var/www/fileshackproject/media/'
+#MEDIA_URL = '/media/'
+#STATIC_ROOT = '/var/www/fileshackproject/static/'
+#STATIC_URL = '/static/'
+
+# Use python -c 'from random import choice; print "".join([choice("abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)") for i in range(50)])'
+# to generate a unique SECRET_KEY.
+SECRET_KEY = 'tcyip!+rjj19acs((0b9w$q*b%q38un7(ng*^f%m*n+j2m8if1'
