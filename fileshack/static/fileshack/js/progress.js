a6f83a04e9262d72513d9769c7f148198b817c15
Peter Kuma <peterkuma@centrum.sk>
2012-03-29 10:41:19 +0200

    Fix progress indication in IE.

--- fileshack/static/fileshack/js/progress.js
+++ fileshack/static/fileshack/js/progress.js
@@ -16,21 +16,30 @@ function updateProgress(pb) {
     var w = 400; // Ugly hack.
     if (pb.value) {
 	pb.removeClass('progress-emulation-indeterminate');
+	var fx = pb.retrieve('fx');
+	if (fx) {
+	    fx.cancel();
+	    pb.store('fx', null);
+	}
 	pb.setStyle('background-position', (pb.value/100*w-1000) + 'px center');
     } else {
 	pb.addClass('progress-emulation-indeterminate');
-	fx = new Fx.Tween(pb, {
-	    duration: 'long',
-	    transition: 'linear',
-	    property: 'background-position',
-	    link: 'chain'
-	});
-	var atStart = true;
-	fx.addEvent('complete', function() {
-	    if (atStart) fx.start(-1000+w-100);
-	    else fx.start(-1000);
-	    atStart = !atStart;
-	});
-	fx.start(-1000, -1000+w-100);
+	var fx = pb.retrieve('fx');
+	if (!fx) {
+	    fx = new Fx.Tween(pb, {
+		duration: 'long',
+		transition: 'linear',
+		property: 'background-position',
+		link: 'chain'
+	    });
+	    pb.store('fx', fx);
+	    var atStart = true;
+	    fx.addEvent('complete', function() {
+		if (atStart) fx.start(-1000+w-100);
+		else fx.start(-1000);
+		atStart = !atStart;
+	    });
+	    fx.start(-1000, -1000+w-100);
+	}
     }
 }
