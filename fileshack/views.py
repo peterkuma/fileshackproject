peterkuma/fileshackproject

commit 5d31f9abfbf4d3dd06663062ff83f0d12439d6c1

Allow an empty access code.
When access code is empty, access to Store is unrestricted. Logout
button is not shown.

fileshack/views.py
@@ -98,7 +98,8 @@ def logout(request, store):
 @require_store
 def index(request, store):
     if (not request.session.has_key("fileshack_stores") or \
-        not store.id in request.session["fileshack_stores"]):
+        not store.id in request.session["fileshack_stores"]) \
+        and store.accesscode != "":
         
         if request.method == "POST":
             accesscode = request.POST.get("accesscode")
@@ -125,6 +126,7 @@ def index(request, store):
     items = Item.objects.filter(store=store)    
     t = loader.get_template("fileshack/index.html")
     c = RequestContext(request, {
+        "store": store,
         "items": items,
         "item_size_limit": store.item_limit,
         "items_json": JSONEncoder().encode([i.simple() for i in items])

fileshack/models.py
@@ -32,7 +32,7 @@ from datetime import timedelta
 class Store(Model):
     path = CharField(_("path"), help_text=_("Path relative to the base URL under which the store is accessible. Leave empty if not sure."), unique=True, blank=True, max_length=200)
     media = CharField(_("media"), help_text=_("Directiory under MEDIA_PATH into which files will be uploaded. Leave empty if not sure."), blank=True, max_length=200)
-    accesscode = CharField(_("access code"), help_text=_("Protect access to the store by an access code."), max_length=200)
+    accesscode = CharField(_("access code"), help_text=_("Protect access to the store by an access code."), max_length=200, blank=True)
     item_limit = IntegerField(_("item size limit"), help_text=_("Limit the size of a single file."), default=0)
     store_limit = IntegerField(_("store size limit"), help_text=_("Limit the size of the entire store."), default=0)
     protect_files = BooleanField(_("protect files"), help_text=_("Protect files by a random string, so that they cannot be downloaded by guessing their name."), default=True)

fileshack/templates/fileshack/index.html
@@ -28,9 +28,11 @@
     <body>
         <div id="container">
 	    <div id="controls">
-                <form action="logout/">
-                    <button type="submit" class="logout">{% trans "Logout" %}</button>
-                </form>
+		{% if store.accesscode %}
+		    <form action="logout/">
+			<button type="submit" class="logout">{% trans "Logout" %}</button>
+		    </form>
+		{% endif %}
             </div>
             <h1>{%trans "Welcome to fileshack" %}, <span>{% trans "drop your items" %}</span></h1>
             <div id="subcontainer">

commit a1cfeced39e3902c9840f608640878b488f36689

Fix bug introduced in 5d31f9abfb.
require_login has to be updated to work with stores with an empty access
code.

fileshack/views.py
@@ -76,14 +76,17 @@ def require_store(view):
 def require_login(view):
     def login_wrapper(request, *args, **kwargs):
         store = kwargs.get("store")
+        if not store: return Http404()
         
-        if store == None:
-            return HttpResponseForbidden("No store defined")
-        if not request.session.has_key("fileshack_stores") or \
-           not store.id in request.session["fileshack_stores"]:
-            return HttpResponseForbidden("Not logged in")
-            
-        return view(request, *args, **kwargs)
+        if store.accesscode == "":
+            return view(request, *args, **kwargs)
+        
+        if request.session.has_key("fileshack_stores") and \
+           store.id in request.session["fileshack_stores"]:
+            return view(request, *args, **kwargs)
+        
+        return HttpResponseNotAllowed()
+        
     return login_wrapper
 
 @require_store