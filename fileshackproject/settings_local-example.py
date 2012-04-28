097ed8d05b07a073215400f27f63f6cdeb06b6c7
Peter Kuma <peterkuma@centrum.sk>
2012-04-28 18:15:42 +0200

    Change FILESHACK_EMAIL FROM in settings example.
    Better to suggest no-reply@appid.appspotmail.com than
    no-reply@example.org.

--- fileshackproject/settings_local-example.py
+++ fileshackproject/settings_local-example.py
@@ -7,7 +7,7 @@
 #TIME_ZONE = 'UTC'
 
 # E-mail address from which notifications are sent.
-#FILESHACK_EMAIL_FROM = 'no-reply@example.org'
+#FILESHACK_EMAIL_FROM = 'no-reply@appid.appspotmail.com'
 
 # Fileshack: Be sure to fill in a unique SECRET_KEY. Otherwise recipients
 # of e-mail updates will not be able to unsubscribe by following a link.
