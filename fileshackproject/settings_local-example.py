702c9d67d548f0be021321fff302d0f034a76d18
Peter Kuma <peter@peterkuma.net>
2021-08-03 14:22:47 +0200

    Update SECRET_KEY settings command to Python 3

--- fileshackproject/settings_local-example.py
+++ fileshackproject/settings_local-example.py
@@ -45,6 +45,6 @@
 # Fileshack: Be sure to fill in a unique SECRET_KEY. Otherwise recipients
 # of e-mail updates will not be able to unsubscribe by following a link.
 #
-# Use python -c 'from random import choice; print "".join([choice("abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)") for i in range(50)])'
+# Use python3 -c 'from random import choice; print("".join([choice("abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)") for i in range(50)]))'
 # to generate a unique SECRET_KEY.
 #SECRET_KEY = ''
