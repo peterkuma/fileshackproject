3917eb457ac3f3ad9e874a238b3aaffa475fba6f
Peter Kuma <peter@peterkuma.net>
2021-08-05 11:51:38 +0200

    Make wsgi module compatible with new Django

--- fileshackproject/wsgi.py
+++ fileshackproject/wsgi.py
@@ -1,33 +1,16 @@
 """
-WSGI config for fileshackproject.
+WSGI config for fileshackproject project.
 
-This module contains the WSGI application used by Django's development server
-and any production WSGI deployments. It should expose a module-level variable
-named ``application``. Django's ``runserver`` and ``runfcgi`` commands discover
-this application via the ``WSGI_APPLICATION`` setting.
+It exposes the WSGI callable as a module-level variable named ``application``.
 
-Usually you will have the standard Django WSGI application here, but it also
-might make sense to replace the whole Django WSGI application with a custom one
-that later delegates to the Django one. For example, you could introduce WSGI
-middleware here, or combine a Django application with an application of another
-framework.
-
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
+
 import os
-import sys
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fileshackproject.settings")
-sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+from django.core.wsgi import get_wsgi_application
 
-# This application object is used by any WSGI server configured to use this
-# file. This includes Django's development server, if the WSGI_APPLICATION
-# setting points here.
-#from django.core.wsgi import get_wsgi_application
-#application = get_wsgi_application()
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fileshackproject.settings')
 
-import django.core.handlers.wsgi
-application = django.core.handlers.wsgi.WSGIHandler()
-
-# Apply WSGI middleware here.
-# from helloworld.wsgi import HelloWorldApplication
-# application = HelloWorldApplication(application)
+application = get_wsgi_application()
